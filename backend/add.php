96a1fbf1e1da8c00814731f8f3b0f7ce9e317537
unknown <hanadi.hosri@gmail.com>
2024-03-17 14:12:41 +0200

    finalizing add.php

--- backend/add.php
+++ backend/add.php
@@ -1,16 +1,17 @@
 <?php
 include('connection.php');
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
-
 $details = $_POST['details'];
 
 $query = $mysqli->prepare('insert into stories(details) values(?);');
 $query->bind_param('s', $details);
-$query->execute();
-$response['status'] = "success";
-$response['message'] = "story was added successfully";
+if ($query->execute()){
+    $response['status'] = "success";
+    $response['message'] = "story was added successfully";
+} else {
+    $response['status'] = "failed";
+    $response['message'] = "story wasn't added";
+}
+
 
 echo json_encode($response);
