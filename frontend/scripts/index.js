985d0e1fdfb5e6710b0f2bd0787b0047df8b2024
unknown <hanadi.hosri@gmail.com>
2024-03-17 06:05:02 +0200

    fetching data

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,1 +1,15 @@
-console.log("hello");
+const news = [];
+
+fetch("http://localhost/news-website/backend/read.php", {
+    method: "GET",
+     headers: {
+        "Access-Control-Allow-Origin": "*"
+     }
+    })
+    .then(response => response.json())
+    .then(data => {
+        news.push(data.message.details)
+    })
+    .catch(error => {console.error(error);});
+
+console.log(news);
